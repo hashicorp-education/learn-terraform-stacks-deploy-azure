b06075646c0476b15327123bf0336113e191bbd5
Robin Norwood <rnorwood@hashicorp.com>
2025-09-19 12:47:19 -0500

    Fix output values

--- storage/outputs.tf
+++ storage/outputs.tf
@@ -6,7 +6,17 @@ output "storage_container_name" {
   value       = azurerm_storage_container.function_container.name
 }
 
-output "primary_connection_string" {
+output "storage_account_name" {
+  description = "The name of the storage account."
+  value       = azurerm_storage_account.function_storage.name
+}
+
+output "storage_account_primary_access_key" {
+  description = "The primary access key of the storage account."
+  value       = azurerm_storage_account.function_storage.primary_access_key
+}
+
+output "storage_account_primary_connection_string" {
   description = "Storage account connection string"
   value = azurerm_storage_account.function_storage.primary_connection_string
 }
