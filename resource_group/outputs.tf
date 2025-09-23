7a9decc54da1e18b21c54fcbff6508e2cb4bdb87
Robin Norwood <rnorwood@hashicorp.com>
2025-09-23 17:18:41 -0500

    Random provider

--- resource_group/outputs.tf
+++ resource_group/outputs.tf
@@ -1,9 +1,9 @@
 output "resource_group_name" {
   description = "The name of the resource group."
-  value       = azurerm_resource_group.function.name
+  value       = azurerm_resource_group.stacks.name
 }
 
 output "resource_group_location" {
   description = "The location of the resource group."
-  value       = azurerm_resource_group.function.location
+  value       = azurerm_resource_group.stacks.location
 }
