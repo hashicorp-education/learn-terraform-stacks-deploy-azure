416bfa81a7d8d81bcb5abc20976d290a72f9ecc9
Robin Norwood <rnorwood@hashicorp.com>
2025-09-23 19:49:26 -0500

    output instance info

---
+++ instance/outputs.tf
@@ -0,0 +1,13 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+output "instance_ids" {
+  description = "IDs of Linux virtual machines."
+  value       = azurerm_linux_virtual_machine.private[*].id
+}
+
+# Private IP addresses of the VMs
+output "private_ips" {
+  description = "Private IP addresses of Linux virtual machines."
+  value       = azurerm_network_interface.private.*.ip_configuration[0].private_ip_address
+}
